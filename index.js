88d51bc2a1751709037f91e34735fb1781213cc9
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-21 21:37:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 function findMatching(drivers,string){
 return drivers.filter(function(st){
     return string.toLowerCase() === st.toLowerCase();
-  }
+  })
 }
 
 //function findMatching(drivers,string){
@@ -43,5 +43,5 @@ function fuzzyMatch(drivers,string){
 const lengthof = string.length;
 return drivers.filter(function(st){
 return string === st.slice(0,lengthof);
- }
+ })
 }
