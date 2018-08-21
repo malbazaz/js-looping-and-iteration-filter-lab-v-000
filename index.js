8d21eeaa8ec4600f54a5c682330ae3b791f0a2b1
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-21 20:54:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 // fingmatching take array of drivers
 // and string as arguments
 // return matching list of drivers
-function findMatching(array,string){
+function findMatching(drivers,string){
 const newArray;
-for(const st of array){
+for(const st of drivers){
   let st1 = st.toLowerCase();
   let st2 = string.toLowerCase();
   if(st1 === st2){
